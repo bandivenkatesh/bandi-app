9b70c396b94fbe8bad349d957cc543ec0ddef7e8
bandivenkatesh <bandivenky2222@gmail.com>
2025-01-20 15:10:35 +0530

    Remove Custom 404 and 500 page components

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,1 @@
-import { NextPage } from 'next';
-
-const Custom404: NextPage = () => {
-    return (
-        <div>
-            <h1>404 - Page Not Found</h1>
-        </div>
-    );
-};
-
-export default Custom404;
+//
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,12 +1,1 @@
-
-import { NextPage } from 'next';
-
-const Custom500: NextPage = () => {
-    return (
-        <div>
-            <h1>500 - Server-side error occurred</h1>
-        </div>
-    );
-};
-
-export default Custom500;
+//
